e6459c94ff9ea244f797fe82eb3b3add7e43146d
Md. Mamunuz Zaman Sarker <mzaman@insightintechnology.com>
2022-09-13 17:03:43 +0600

    RMFS : Module based routing added

--- UI/Web/Angular/DemoPortfolio/src/app/app-routing.module.ts
+++ UI/Web/Angular/DemoPortfolio/src/app/app-routing.module.ts
@@ -5,18 +5,12 @@ import { AboutComponent } from './pages/about/about.component';
 import { AngularBasicsComponent } from './pages/angular-basics/angular-basics.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ForgetPasswordComponent } from './pages/user-auth/forget-password/forget-password.component';
-import { LoginComponent } from './pages/user-auth/login/login.component';
-import { RegisterComponent } from './pages/user-auth/register/register.component';
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "contact", component: ContactComponent },
   { path: "angular-basics", component: AngularBasicsComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "forget-password", component: ForgetPasswordComponent },
 ];
 
 @NgModule({
--- UI/Web/Angular/DemoPortfolio/src/app/pages/user-auth/user-auth-routing.module.ts
+++ UI/Web/Angular/DemoPortfolio/src/app/pages/user-auth/user-auth-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "forget-password", component: ForgetPasswordComponent },
 ];
 
 @NgModule({
