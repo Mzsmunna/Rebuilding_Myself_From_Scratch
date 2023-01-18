e805f0b712e3c429340ccbec8bd44987578807d7
Md. Mamunuz Zaman Sarker <mzaman@insightintechnology.com>
2023-01-18 22:31:59 +0600

    CPC

---
+++ Skills/CP/RegexMatchPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CP
+{
+    public static class RegexMatchPattern
+    {
+        public static bool IsMatch(string s, string p)
+        {
+
+            bool result = false;
+
+            try
+            {
+                //result = Regex.IsMatch(s, @p);
+
+                Regex rx = new Regex(@p);
+                Match m = rx.Match(s);
+
+                if (m.Length == s.Length)
+                    result = true;
+                else
+                    result = false;
+            }
+            catch (Exception ex)
+            {
+
+                //Console.WriteLine(ex.Message);
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
