be53d68de94ec0a91730abcddd40bf86d03a8709
Mamunuz Zaman <mzs.munna@gmail.com>
2023-05-03 06:58:26 +0600

    CricMz || Entity / Table & System design updated

--- Domain/Entities/CricMz/UserUniverse.cs
+++ Domain/Entities/CricMz/UserUniverse.cs
@@ -22,22 +22,23 @@ namespace Domain.Entities.CricMz
         public string Mobile { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string About { get; set; } = string.Empty;
+        public string Quote { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
         public string CoverImage { get; set; } = string.Empty;
         public string Occupation { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
-        public string Actions { get; set; } = "R"; // "CRUDS", "C", "R", "U", "D", "S", "CR", "UD", "CRU", "RU", "RD", "CD", etc
-        public AdditionalInfo? AdditionalInfo { get; set; } = null;
-        public SocialInfo? SocialInfo { get; set; } = null;
-        public PaymentCardInfo? PaymentCardInfo { get; set; } = null;
-        public BillingInfo? BillingInfo { get; set; } = null;
-        public List<Nationality>? Nationalities { get; set; } = null;
-        public Career? Career { get; set; } = null;
-        public List<Permission> Permissions { get; set; } = new List<Permission>();
-        public TechnicalInfo? TechnicalInfo { get; set; } = new TechnicalInfo();
+        public string Permissions { get; set; } = "R"; // "CRUDS", "C", "R", "U", "D", "S", "CR", "UD", "CRU", "RU", "RD", "CD", etc
+        public UserAdditionalInfo? AdditionalInfo { get; set; } = null;
+        public UserSocialInfo? SocialInfo { get; set; } = null;
+        public UserPaymentCardInfo? PaymentCardInfo { get; set; } = null;
+        public UserBillingInfo? BillingInfo { get; set; } = null;
+        public List<UserNationality>? Nationalities { get; set; } = null;
+        public UserLife? Life { get; set; } = null;
+        public List<UserAuthorization> Authorizations { get; set; } = new List<UserAuthorization>();
+        public UserTechnicalInfo? TechnicalInfo { get; set; } = new UserTechnicalInfo();
     }
 
-    public class Place
+    public class UserPlace
     {
         public string Location { get; set; } = string.Empty;
         public string Lat { get; set; } = string.Empty;
@@ -51,7 +52,7 @@ namespace Domain.Entities.CricMz
         public string Zip { get; set; } = string.Empty;
     }
 
-    public class Nationality
+    public class UserNationality
     {
         public string Visa { get; set; } = string.Empty;
         public string Passport { get; set; } = string.Empty;
@@ -61,7 +62,7 @@ namespace Domain.Entities.CricMz
         public bool IsPermanent { get; set; } = false;
     }
 
-    public class PaymentCardInfo
+    public class UserPaymentCardInfo
     {
         // Credit card details
         public string CardNumber { get; set; } = string.Empty;
@@ -74,7 +75,7 @@ namespace Domain.Entities.CricMz
         public int ExpirationYear { get; set; }
     }
 
-    public class BillingInfo
+    public class UserBillingInfo
     {
         // Billing address
         public string Address { get; set; } = string.Empty;
@@ -87,17 +88,17 @@ namespace Domain.Entities.CricMz
         public string PaymentStatus { get; set; } = string.Empty;
     }
 
-    public class AdditionalInfo
+    public class UserAdditionalInfo
     {
         public string FirstName { get; set; } = string.Empty; // Mamunuz
         public string MiddleName { get; set; } = string.Empty; // Zaman
         public string LastName { get; set; } = string.Empty; // Sarker
         public string NickName { get; set; } = string.Empty; // Munna
         public string TitleName { get; set; } = string.Empty; // Mzs || Mzsmunna
-        public List<ContractInfo>? Contracts { get; set; } = null;
-        public List<AddressInfo>? Addresses { get; set; } = null;
-        public Place? BirthPlace { get; set; } = null;
-        public Place? DeathPlace { get; set; } = null;
+        public List<UserContractInfo>? Contracts { get; set; } = null;
+        public List<UserAddressInfo>? Addresses { get; set; } = null;
+        public UserPlace? BirthPlace { get; set; } = null;
+        public UserPlace? DeathPlace { get; set; } = null;
         public double Height { get; set; } = 0;
         public double Weight { get; set; } = 0;
         public bool IsLeftie { get; set; } = false;
@@ -113,7 +114,7 @@ namespace Domain.Entities.CricMz
         public string CurrentMood { get; set; } = string.Empty; // ","
     }
 
-    public class AddressInfo
+    public class UserAddressInfo
     {
         public string Address { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
@@ -123,7 +124,7 @@ namespace Domain.Entities.CricMz
         public string Zip { get; set; } = string.Empty;
     }
 
-    public class ContractInfo
+    public class UserContractInfo
     {
         public string Category { get; set; } = string.Empty; // "home" / "office" / "personal" / "other"
         public string Email { get; set; } = string.Empty;
@@ -133,7 +134,7 @@ namespace Domain.Entities.CricMz
         public bool IsActive { get; set; } = false;
     }
 
-    public class SocialInfo
+    public class UserSocialInfo
     {
         public string LinkedInProfile { get; set; } = string.Empty;
         public string GitHubProfile { get; set; } = string.Empty;
@@ -148,17 +149,19 @@ namespace Domain.Entities.CricMz
         public string TwitchProfile { get; set; } = string.Empty;
         public string SlackProfile { get; set; } = string.Empty;
         public string ZoomProfile { get; set; } = string.Empty;
-        public List<Link>? Others { get; set; } = null;
+        public List<UserAccessLink>? Others { get; set; } = null;
     }
 
-    public class Link
+    public class UserAccessLink
     {
         public string Id { get; set; } = string.Empty;
         public string ParentID { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
         public string Sections { get; set; } = string.Empty; // ","
-        public string Actions { get; set; } = "R"; // "CRUDS", "C", "R", "U", "D", "S", "CR", "UD", "CRU", "RU", "RD", "CD", etc
+        public string Permissions { get; set; } = "R"; // "CRUDS", "C", "R", "U", "D", "S", "CR", "UD", "CRU", "RU", "RD", "CD", etc
         public string BaseUrl { get; set; } = string.Empty;
         public string DomainUrl { get; set; } = string.Empty;
         public string SubUrl { get; set; } = string.Empty;
@@ -168,18 +171,19 @@ namespace Domain.Entities.CricMz
         public int OrderNo { get; set; } = 0;
     }
 
-    public class Profession
+    public class UserProfession
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty; // "Job", "Business", "Entrepreneur", "Intrapreneur", "Sports", "Music", "Film", "Tourist" etc
-        public string OrganizationID { get; set; } = string.Empty;
-        public string OrganizationName { get; set; } = string.Empty;
+        public string LogisticID { get; set; } = string.Empty;
+        public string LogisticName { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
         public string Designation { get; set; } = string.Empty;
         public string Position { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
@@ -191,102 +195,204 @@ namespace Domain.Entities.CricMz
         public bool IsSelfEmployed { get; set; } = false;
     }
 
-    public class Education
+    public class UserEducation
     {
         public string InstitutionID { get; set; } = string.Empty;
         public string InstitutionName { get; set; } = string.Empty;
+        public string InstitutionType { get; set; } = string.Empty;
+        public string InstitutionFrom { get; set; } = string.Empty;
         public string Degree { get; set; } = string.Empty;
         public string Department { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Grade { get; set; } = string.Empty;
+        public string Class { get; set; } = string.Empty;
+        public string Result { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
     }
 
-    public class Certification
+    public class UserCertification
     {
+        public string Id { get; set; } = string.Empty;
         public string CredentialID { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty; // Institution, Logistic, User, etc
         public string IssuerID { get; set; } = string.Empty;
         public string IssuerName { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime IssuedDate { get; set; }
         public DateTime ExpirationDate { get; set; }
     }
 
-    public class Skill
+    public class UserSkill
     {
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
-        public string Category { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
-        public string LearnedFrom { get; set; } = string.Empty; // Institution, Organization, User, etc
-        public string LearnedFromID { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public string LearnedFrom { get; set; } = string.Empty; // Institution, Logistic, User, etc    
         public string LearnedFromName { get; set; } = string.Empty;
+        public string LearnedFromID { get; set; } = string.Empty;
+        public string CertificationID { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public bool IsSelfLearned { get; set; } = false;
         public DateTime LearnedDate { get; set; }
     }
 
-    public class Project
+    public class UserProject
     {
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
         public string ToolAndTechs { get; set; } = string.Empty; // "C#, Asp.net Core, MongoDB, Reddis, SignalR, RabbitMQ, TypeScript" etc
+        public string Caption { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Link { get; set; } = string.Empty;
+        public List<string> Images { get; set; } = new List<string>();
         public string Location { get; set; } = string.Empty;
         public bool IsSideProject { get; set; } = false;
         public bool IsOpenSourced { get; set; } = false;
         public bool IsContributor { get; set; } = false;
-        public string OrganizationId { get; set; } = string.Empty;
-        public string OrganizationName { get; set; } = string.Empty;
+        public string LogisticId { get; set; } = string.Empty;
+        public string LogisticName { get; set; } = string.Empty;
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
     }
 
-    public class Career
-    {
-        //public List<string>? TimeLineIDs { get; set; } = null; // Should be new Entity collection with user ID
-        public List<Profession>? AllProfessions { get; set; } = null;
-        public List<Education>? Educations { get; set; } = null;
-        public List<Certification>? Certifications { get; set; } = null;
-        public List<Skill>? Skills { get; set; } = null;
-        public List<Project>? Projects { get; set; } = null;
-    }
-
-    public class Permission
-    {
-        public Role AppRole { get; set; } = new Role();
-        public List<Organization>? Organizations { get; set; } = null; // pages, groups, shops, stores, groceries etc
-    }
-
-    public class Organization
+    public class UserAchievement
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
-        public bool IsFounder { get; set; } = false;
-        public bool IsSharedOwner { get; set; } = false;
-        public int Ownerships { get; set; } = 0; // 0 - 100%
-        public List<Role> Roles { get; set; } = new List<Role>();
+        public string Title { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public List<string>? Images { get; set; } = null;
+        public List<string>? Videos { get; set; } = null;
+        public List<string>? Attachments { get; set; } = null;
+        public string Icon { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsMilestone { get; set; } = false;
+        public DateTime AchievedDate { get; set; }
     }
 
-    public class Role
+    public class UserMemoryAndMoment
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty; // Memorable, Funny, Epic, Weird, Awkward, Confusing, Incredible, Amaizing, Awesome, Remarkable, Lovely, Happy, Exciting, Horrifying, Terrific, Scary
+        public string Category { get; set; } = string.Empty;
+        public List<string>? Images { get; set; } = null;
+        public List<string>? Videos { get; set; } = null;
+        public List<string>? Attachments { get; set; } = null;
+        public List<string>? Withs { get; set; } = null;
+        public string Caption { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime MomentDate { get; set; }
+    }
+
+    public class UserFriendAndFamily
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string UserImage { get; set; } = string.Empty;
+        public string UserCover { get; set; } = string.Empty;
+        public string Relation { get; set; } = string.Empty; // Friend, Family, Colleague, Wife, Husband, Mom, Dad, Brother, Sister, Uncle, Aunties,  etc
+        public List<string>? Images { get; set; } = null;
+        public List<string>? Videos { get; set; } = null;
+        public List<string>? Attachments { get; set; } = null;
+        public bool IsBloodRelated { get; set; } = false;
+        public bool IsClose { get; set; } = false;
+        public bool IsBestie { get; set; } = false;
+        public bool IsInspiration { get; set; } = false;
+        public bool IsGuider { get; set; } = false;
+        public DateTime Since { get; set; }
+        public DateTime? Till { get; set; }
+    }
+
+    public class UserFollow
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty; // user, logistic, etc
+        public string Category { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Cover { get; set; } = string.Empty;
+        public bool IsFavourite { get; set; } = false;
+        public bool IsInspiration { get; set; } = false;
+        public bool IsFamus { get; set; } = false;
+        public bool IsCelebrity { get; set; } = false;
+        public bool IsSubscribed { get; set; } = false;
+        public int Rank { get; set; } = 0;
+        public DateTime Since { get; set; }
+        public DateTime? Till { get; set; }
+    }
+
+    public class UserCareer
+    {
+        public List<UserProfession>? Professions { get; set; } = null;
+        public List<UserEducation>? Educations { get; set; } = null;
+        public List<UserCertification>? Certifications { get; set; } = null;
+        public List<UserSkill>? Skills { get; set; } = null;
+        public List<UserProject>? Projects { get; set; } = null;
+        public List<UserAchievement>? Achievements { get; set; } = null;
+    }
+
+    public class UserLife
+    {
+        //public List<string>? TimeLineIDs { get; set; } = null; // Should be new Entity collection with user ID
+        public UserCareer? Career { get; set; } = null;
+        public List<UserFriendAndFamily>? FriendAndFamilies { get; set; } = null;
+        public List<UserMemoryAndMoment>? MemoryAndMoments { get; set; } = null;
+        public List<UserFollow>? Followers { get; set; } = null;
+        public List<UserFollow>? Followings { get; set; } = null;
+    }
+
+    public class UserAuthorization
+    {
+        public UserRole AppRole { get; set; } = new UserRole();
+        public List<UserLogistic>? Logistics { get; set; } = null; // pages, groups, shops, stores, groceries etc
+    }
+
+    public class UserLogistic
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public bool IsFounder { get; set; } = false;
+        public bool IsOwner { get; set; } = false;
+        public bool IsSharedOwner { get; set; } = false;
+        public int Ownerships { get; set; } = 0; // 0 - 100%
+        public List<UserRole> Roles { get; set; } = new List<UserRole>();
+    }
+
+    public class UserRole
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty; // "admin", "moderator", "agent", "manager", "user"
-        public string Category { get; set; } = string.Empty; // "paid", "subscribed", "in_app_purchase" etc
         public string Title { get; set; } = string.Empty;
-        public List<Link> AuthorizedUrls { get; set; } = new List<Link>();
+        public string Type { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty; // "paid", "subscribed", "in_app_purchase" etc        
+        public string Permissions { get; set; } = "R"; // "CRUDS", "C", "R", "U", "D", "S", "CR", "UD", "CRU", "RU", "RD", "CD", etc
+        public List<UserAccessLink> AuthorizedUrls { get; set; } = new List<UserAccessLink>();
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public bool IsAdmin { get; set; } = false;
         public bool IsActive { get; set; } = false;
     }
 
-    public class TechnicalInfo
+    public class UserTechnicalInfo
     {
         public string RefreshToken { get; set; } = string.Empty;
         public string ConfirmationCode { get; set; } = string.Empty;
