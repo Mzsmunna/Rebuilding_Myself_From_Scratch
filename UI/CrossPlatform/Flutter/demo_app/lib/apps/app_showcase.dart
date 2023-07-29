7d2eed974e9331316e642d6676ca8fbad9d03e32
Mamunuz Zaman <mzs.munna@gmail.com>
2023-07-29 20:53:25 +0600

    connectivity plus, shared preference & dio packages added

--- UI/CrossPlatform/Flutter/demo_app/lib/apps/app_showcase.dart
+++ UI/CrossPlatform/Flutter/demo_app/lib/apps/app_showcase.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:demo_app/common/configs/themes/default_app_theme.dart';
 import 'package:demo_app/common/layouts/app_drawer/app_default_drawer.dart';
 import 'package:demo_app/common/widgets/scrolling_gesture_detector_cards_column_widget.dart';
@@ -5,8 +8,54 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 //import '../../widgets/access_widgets.dart';
 
-class AppShowcase extends StatelessWidget {
+class AppShowcase extends StatefulWidget {
   const AppShowcase({super.key});
+
+  @override
+  State<AppShowcase> createState() => _AppShowcaseState();
+}
+
+class _AppShowcaseState extends State<AppShowcase> {
+  late StreamSubscription<ConnectivityResult> connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    connectivitySubscription = Connectivity().onConnectivityChanged.listen(
+      (ConnectivityResult connectivityResult) {
+        //print("connectivityResult....");
+        if (connectivityResult == ConnectivityResult.none) {
+          //print("no internet");
+          // I am not connected to any network.
+        } else {
+          //print("success");
+          if (connectivityResult == ConnectivityResult.mobile) {
+            // I am connected to a mobile network.
+          } else if (connectivityResult == ConnectivityResult.wifi) {
+            // I am connected to a wifi network.
+          } else if (connectivityResult == ConnectivityResult.ethernet) {
+            // I am connected to a ethernet network.
+          } else if (connectivityResult == ConnectivityResult.vpn) {
+            // I am connected to a vpn network.
+            // Note for iOS and macOS:
+            // There is no separate network interface type for [vpn].
+            // It returns [other] on any device (also simulator)
+          } else if (connectivityResult == ConnectivityResult.bluetooth) {
+            // I am connected to a bluetooth.
+          } else if (connectivityResult == ConnectivityResult.other) {
+            // I am connected to a network which is not in the above mentioned networks.
+          }
+        }
+      },
+    );
+  }
+
+  // Be sure to cancel subscription after you are done
+  @override
+  dispose() {
+    connectivitySubscription.cancel();
+    super.dispose();
+  }
 
   // This widget is the root of your application.
   @override
